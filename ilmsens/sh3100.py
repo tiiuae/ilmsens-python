77ff7e8ab35cde457cc7578aacdc18e0f245dc7e
sabughazal <sultan.abughazal@tii.ae>
2023-12-19 10:04:42 +0400

    Initial commit

--- ilmsens/sh3100.py
+++ ilmsens/sh3100.py
@@ -1,8 +1,16 @@
-
+import os
+from ctypes import cdll
 
 class MEXPLORE_SH3100():
+
+    LIB_PATH = os.path.join("usr", "lib", "libilmsens_hal.so")
+
     def __init__(self) -> None:
-        pass
+        assert os.path.exists(self.LIB_PATH)
+        lib = cdll.LoadLibrary(self.LIB_PATH)
+        lib.ilmsens_hal_initHAL()
 
     def get_serial_number(self):
         return "serial_number"
+
+
